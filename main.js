d25ef06df887982175055d5350266bb3ab85b759
ThatKit <emper137137@gmail.com>
2021-05-05 19:34:46 +1000

    level units animation

--- main.js
+++ main.js
@@ -17,20 +17,47 @@ workCards[2].onmouseout = () => colouringFilm[2].style.opacity = '';
 
 const skillCards = document.getElementsByClassName('skills-card');
 const skillIcons = document.getElementsByClassName('skills-card-icon-container');
+const skillLevels = document.getElementsByClassName('level-fraction');
 
 skillCards[0].onmouseover = () => {
     skillIcons[0].style.background = 'url("../personal-portfolio-page/img/icons/html-colour.svg")';
     skillIcons[1].style.background = 'url("../personal-portfolio-page/img/icons/css-colour.svg")';
+    for (let i = 0; i < 7; i++) {
+        skillLevels[i].style.background = '#C79F4E00';
+    }
 }
-skillCards[1].onmouseover = () => skillIcons[2].style.background = 'url("../personal-portfolio-page/img/icons/js-colour.svg")';
-skillCards[2].onmouseover = () => skillIcons[4].style.background = 'url("../personal-portfolio-page/img/icons/figma-colour.svg")';
+skillCards[1].onmouseover = () => {
+    skillIcons[2].style.background = 'url("../personal-portfolio-page/img/icons/js-colour.svg")';
+    for (let i = 7; i < 13; i++) {
+        skillLevels[i].style.background = '#C79F4E00';
+    }
+}
+skillCards[2].onmouseover = () => {
+    skillIcons[4].style.background = 'url("../personal-portfolio-page/img/icons/figma-colour.svg")';
+    for (let i = 13; i < 19; i++) {
+        skillLevels[i].style.background = '#C79F4E00';
+    }
+}
 
 skillCards[0].onmouseout = () => {
     skillIcons[0].style.background = '';
     skillIcons[1].style.background = '';
+    for (let i = 0; i < 7; i++) {
+        skillLevels[i].style.background = '';
+    }
 }
-skillCards[1].onmouseout = () => skillIcons[2].style.background = '';
-skillCards[2].onmouseout = () => skillIcons[4].style.background = '';
+skillCards[1].onmouseout = () => {
+    skillIcons[2].style.background = '';
+    for (let i = 7; i < 13; i++) {
+        skillLevels[i].style.background = '';
+    }
+}
+skillCards[2].onmouseout = () => {
+    skillIcons[4].style.background = '';
+    for (let i = 13; i < 19; i++) {
+        skillLevels[i].style.background = '';
+    }
+}
 
 // Contacts hover animation
 

79036ec766ed4010d1b68e55b8f3ef992229e52c
ThatKit <emper137137@gmail.com>
2021-05-05 17:42:58 +1000

    skills cards hover animation with js initial changes

--- main.js
+++ main.js
@@ -12,3 +12,12 @@ workCards[2].onmouseover = () => colouringFilm[2].style.opacity = 0;
 workCards[0].onmouseout = () => colouringFilm[0].style.opacity = '';
 workCards[1].onmouseout = () => colouringFilm[1].style.opacity = '';
 workCards[2].onmouseout = () => colouringFilm[2].style.opacity = '';
+
+// Skills cards hover animation
+
+const skillCardsContainer = document.getElementById('skills-container');
+const skillIcons = document.getElementsByClassName('skills-card-icon-container');
+
+//skillCardsContainer.addEventListener('mouseover', function(event) {
+//    skillIcons[0].style.background = 'url(\'../img/icons/html-colour.svg\')';
+//})
